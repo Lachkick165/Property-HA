9babbfe941b9cc08f4ed5e22fc641e644e2758d2
Lachkick165 <LachTwitch@gmail.com>
2022-08-30 22:57:43 +0200

    fi alle heute

--- src/MainProgrem.java
+++ src/MainProgrem.java
@@ -1,9 +1,25 @@
+import sun.applet.Main;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Time;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MainProgrem {
 
+    JFrame window;
+    JLabel counterLabel;
+    Font font1 = new Font("Arial", Font.PLAIN, 70);
+
+    Timer timer;
+    int second;
+
     public static void main(String[] args) {
+        new MainProgrem();
+
         System.out.println("hihi");
 
         Property p1 = new Property("Erzbergerstraße 1", 20, 20.5);
@@ -20,24 +36,64 @@ public class MainProgrem {
 
         Property p5 = new Property("Erzbergerstraße 5", 100, 100.5);
         System.out.println(p5.getAddress() + " - " + p5.getSize() + "m²");
-
-
-       /*int Timer = 6;
-
-        System.out.println(Timer);
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setBounds(500, 500, 100, 100);
-
-        frame.repaint();
-
-        while (Timer >= 5){
-
-            return;
-        }
-        frame.setVisible(false);*/
     }
 
+    public MainProgrem() {
 
+        window = new JFrame();
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(null);
 
+        counterLabel = new JLabel("");
+        counterLabel.setBounds(300, 230, 200, 100);
+        counterLabel.setHorizontalAlignment(JLabel.CENTER);
+        counterLabel.setFont(font1);
+
+        window.add(counterLabel);
+        window.setVisible(true);
+
+        second = 6;
+        simpleTimer();
+        timer.start();
+    }
+
+    public void simpleTimer(){
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                second--;
+
+                counterLabel.setText(""+ second);
+
+                if(second==0){
+                    second = 7;
+                }
+
+                if(second==6){
+                    timer.stop();
+                }
+            }
+        });
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
